f6b6488c95c653f527b7094d11940790942eb12d
abhivermajeet <139966926+abhivermajeet@users.noreply.github.com>
2023-09-21 18:00:34 +0530

    Delete CourseRepository.java

--- CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.learnSphere.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.learnSphere.entities.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Integer>{
-	Course findByCourseId(int courseId);
-}
